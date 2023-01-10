d47b3f3cde3404569879c7ad216cceb2a386b4df
makkob <maximkabig945@gmail.com>
2023-01-10 22:15:49 +0200

    bot

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const bot = new TelegramApi(TOKEN, { polling: true });
 bot.on("message", (msg) => {
   const text = msg.text;
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `Ти написав мені ${text}`);
+  // bot.sendMessage(chatId, `Ти написав мені ${text}`);
+
+  let random = Math.floor(Math.random() * 100);
+
+  if (random > 50) {
+    bot.sendMessage(
+      chatId,
+      `Ти написав мені : "${text} ", але краще  випий пива`
+    );
+  } else {
+    bot.sendMessage(
+      chatId,
+      `Ти написав мені : " ${text}"  , але краще випий горілки `
+    );
+  }
+
   console.log(msg);
 });
